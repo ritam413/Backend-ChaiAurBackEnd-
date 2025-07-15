a9f8502289fb95e2c1add03e5185925b14a58032
ritam413 <ritamm413@gmail.com>
2025-07-16 02:22:07 +0530

    Accomplished Connection TO DB

---
+++ src/index.js
@@ -0,0 +1,69 @@
+// require('dotenv').config({path:'./env'})
+import connectDB from './db/dbIndex.js'
+import  dotenv  from 'dotenv';
+
+dotenv.config({
+    path:'./env'
+})
+
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import express from 'express';
+// const app = express()
+
+// ;(async ()=>{
+//     try{
+//         await mongoose.connect(`${process.env.MONGODB_URI}/ ${DB_NAME}`)
+//         app.on('error',(error)=>{
+//             console.log('ERROR',error)
+//             throw error
+//         })
+
+//         app.listen(process.env.PORT,()=>{
+//             console.log(`ApP is listening on port ${process.env.PORT}`)
+//         })
+
+//     }catch(error){
+//         console.log('ERROR',error)
+//         throw err
+//     }
+// })()
